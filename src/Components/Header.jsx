6543ad63df9d1b3882c5f89c18c48503a2a422ff
DanielCavalcantih <danielcavalcanti8000@gmail.com>
2024-07-04 20:45:45 -0300

    Ajustes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,8 +4,8 @@ function Header() {
   return (
     <header id="home" className="header-main">
       <div className="container-title">
-        <h1 className="title typing-animation"><span>const</span> <span>name =</span> 'Daniel Cavalcanti';</h1>
-        <p className="subtitle blank"><span>const</span> <span>office =</span> 'Dev Full-Stack';</p>
+        <h1 className="title typing-animation">Daniel Cavalcanti</h1>
+        <p className="subtitle blank">Full-Stack Developer</p>
       </div>
     </header>
   )
